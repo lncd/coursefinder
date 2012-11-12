73c96bef9c482b6780f2673c1d4992efb5872ad4
Jamie Mahoney <jmahoney127@googlemail.com>
2012-11-12 13:39:30 +0000

    Whitespace / tab switcheroo.

--- src/application/models/jacs_model.php
+++ src/application/models/jacs_model.php
@@ -2,23 +2,23 @@
 class Jacs_model extends CI_Model
 {
 	function __construct()
-    {
-        // Call the Model constructor
-        parent::__construct();
-    }
-    
-    function get_course_ids_by_jacs_code($code)
-    {
-        $results = json_decode(file_get_contents('http://n2/course_codes?related_jacs_id=' . $code));
-        $returning = array();
+	{
+		// Call the Model constructor
+		parent::__construct();
+	}
+	
+	function get_course_ids_by_jacs_code($code)
+	{
+		$results = json_decode(file_get_contents('http://n2/course_codes?related_jacs_id=' . $code));
+		$returning = array();
 
-        foreach($results->results as $result)
-        {
-            $returning[] = $result->id;
-        }
+		foreach($results->results as $result)
+		{
+			$returning[] = $result->id;
+		}
 
-        return $returning;
-    }
+		return $returning;
+	}
 }
 
 //EOF
--- src/application/models/keyword_model.php
+++ src/application/models/keyword_model.php
@@ -2,15 +2,15 @@
 class Keyword_model extends CI_Model
 {
 	function __construct()
-    {
-        // Call the Model constructor
-        parent::__construct();
-    }
-    
-    function get_like_keywords_json($term)
-    {
-    	$results = $this->db->like('keyword', $term)->limit(10)->get('keywords');
-    	$json_string = '[';
+	{
+		// Call the Model constructor
+		parent::__construct();
+	}
+	
+	function get_like_keywords_json($term)
+	{
+		$results = $this->db->like('keyword', $term)->limit(10)->get('keywords');
+		$json_string = '[';
 		foreach($results->result() as $result)
 		{
 			$json_string.= '{"id": ' . $result->id . ',"name":"' . $result->keyword . '"},';
@@ -19,53 +19,53 @@ class Keyword_model extends CI_Model
 		$json_string = substr_replace($json_string ,"",-1);
 		$json_string.= ']';
 		return $json_string;
-    }
+	}
 
-    function get_courses_all_keywords($keywords)
-    {
-        $k = new Keyword;
-        $k->where_in('id', $keywords);
-        $k->limit(25);
-        $k->get();
-    }
+	function get_courses_all_keywords($keywords)
+	{
+		$k = new Keyword;
+		$k->where_in('id', $keywords);
+		$k->limit(25);
+		$k->get();
+	}
 
-    function get_course_ids_by_keyword($keyword_id, $relevance = 0.5)
-    {
-        $kc = new Keyword_course_link;
-        $kc->where('keyword_id', (int) $keyword_id);
-        $kc->where('relevance >=', $relevance);
+	function get_course_ids_by_keyword($keyword_id, $relevance = 0.5)
+	{
+		$kc = new Keyword_course_link;
+		$kc->where('keyword_id', (int) $keyword_id);
+		$kc->where('relevance >=', $relevance);
 
-        $links = $kc->get_iterated();
+		$links = $kc->get_iterated();
 
-        $returning = array();
+		$returning = array();
 
-        foreach($links as $link)
-        {
-            $returning[] = $link->stored->course_id;
-        }
+		foreach($links as $link)
+		{
+			$returning[] = $link->stored->course_id;
+		}
 
-        return $returning;
-    }
+		return $returning;
+	}
 
-    function prioritise_results($input)
-    {
-        $all_courses = array();
+	function prioritise_results($input)
+	{
+		$all_courses = array();
 
-        foreach($input as $input_array)
-        {
-            foreach($input_array as $value)
-            {
-                $all_courses[] = $value;
-            }
-        }
+		foreach($input as $input_array)
+		{
+			foreach($input_array as $value)
+			{
+				$all_courses[] = $value;
+			}
+		}
 
-        $id_counts = array_count_values($all_courses);
-        //Sort the ids!
-        arsort($id_counts);
+		$id_counts = array_count_values($all_courses);
+		//Sort the ids!
+		arsort($id_counts);
 
-        $slice = array_slice($id_counts, 0, 25, TRUE);
-        return $slice;
-    }
+		$slice = array_slice($id_counts, 0, 25, TRUE);
+		return $slice;
+	}
 }
 
 //EOF
