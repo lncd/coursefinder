ec5f94cff3bd0bf8a94010fa74897c784142e6fd
Jamie Mahoney <jmahoney127@googlemail.com>
2012-11-15 13:52:42 +0000

    A few tidy ups.

--- src/application/views/results.php
+++ src/application/views/results.php
@@ -21,14 +21,14 @@
 	</section>
 
 	<section class="span4">
-		<div class="well">
+		<!--<div class="well">
 			<p>These search results are based on the following criteria: </p>
 			<ul>
 				<li>Some criteria</li>
 				<li>Shown here</li>
 				<li>And here</li>
-			</ul>
-		</div>
+			
+		</div></ul>-->
 		<p>Search results are generated from the criteria you specified previously.</p>
 		<p>Courses that match the subject areas you specified, as well as the keywords and level of study are returned, and then prioritised based on the amount of criteria they each meet.</p>
 		<p>Some search results are prioritised further when other people have recommended results from similar searches to your specified criteria.</p> 
