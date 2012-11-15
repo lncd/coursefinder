6d82bf4fd4880e416d20b1f2b728840c96a82bba
Jamie Mahoney <jmahoney127@googlemail.com>
2012-11-15 10:45:40 +0000

    Test functions for parameter matching now work.

--- src/application/controllers/home.php
+++ src/application/controllers/home.php
@@ -44,6 +44,26 @@ class Home extends CI_Controller {
 		$this->load->view('home', $data);
 		$this->load->view('footer');
 	}
+
+	public function test()
+	{
+		$this_search = 146;
+		$this_instance = new Search_instance;
+		$this_instance->where('id', $this_search)->get();
+
+		$all_instances = new Search_instance;
+		$all_instances->where('parameter_count', $this_instance->parameter_count)->get_iterated();
+
+		$this->load->model('search_instance_model');
+
+		foreach($all_instances as $an_instance)
+		{
+			if($this->search_instance_model->check_parameters_match(146,$an_instance->id) === 1)
+			{
+				echo 'these match';
+			}
+		}
+	}
 }
 
 // End of file home.php
